632f869a02d88dae9e90e1951e7a8886012d3569
shu12388y <spl.sp999@gmail.com>
2026-02-03 21:01:21 +0530

    added husky

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -204,7 +204,6 @@ const Hero = () => {
           </div>
         </div>
       </section>
-
       <section id="blog" className="py-20">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <motion.div
